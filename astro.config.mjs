ba052f3e06872f93a706d7e1dc89fd1574104c09
Abbas <muhdabbas98@gmail.com>
2024-06-30 00:25:36 +0800

    add partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,11 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-
+import partytown from '@astrojs/partytown';
 import image from '@astrojs/image';
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://ismiabbas.site/',
-  integrations: [mdx(), sitemap(), tailwind(), react(), image()],
+  integrations: [mdx(), sitemap(), tailwind(), react(), image(), partytown()],
 });
