12a50168fc6d9581ba24202d7ab0e39bb19e8339
Abbas <muhdabbas98@gmail.com>
2024-07-23 22:02:41 +0800

    update

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'ismi-abbas.com';
-export const SITE_DESCRIPTION = 'ismi-abbas.com';
+export const SITE_TITLE = 'ismiabbas.site';
+export const SITE_DESCRIPTION = 'ismiabbas.site';
