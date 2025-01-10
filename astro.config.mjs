7258e7d2a4cd65b59ef4d368f14bbeea090281f9
Abbas <abbaspuzi.dev@gmail.com>
2025-01-10 21:17:05 +0800

    :bug: Temporary fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import tailwind from '@astrojs/tailwind';
 import sanity from '@sanity/astro';
 import { defineConfig } from 'astro/config';
 import { loadEnv } from 'vite';
-import node from '@astrojs/node';
 
 const {
   PUBLIC_SANITY_STUDIO_PROJECT_ID,
@@ -22,10 +21,6 @@ const dataset = PUBLIC_SANITY_STUDIO_DATASET || PUBLIC_SANITY_DATASET;
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.ismiabbas.xyz/',
-  output: 'server',
-  adapter: node({
-    mode: 'standalone',
-  }),
   integrations: [
     mdx(),
     sitemap(),
